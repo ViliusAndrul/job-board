c6a66fcc6310b1b3655499ff8676930bb97bf526
ViliusAndrul <vilius.andrulevicius@gmail.com>
2025-06-03 22:59:05 +0300

    Added job routes and connected jobController with middleware

---
+++ routes/jobs.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    createJob,
+    getAllJobs,
+    getJobById,
+    updateJob,
+    deleteJob
+} = require('../controllers/jobController');
+
+const authMiddleware = require('../middleware/authMiddleware');
+const roleMiddleware = require('../middleware/roleMiddleware');
+
+//public route
+router.get('/', getAllJobs);
+router.get('/:id', getJobById);
+
+//protected routes
+router.post('/', authMiddleware, roleMiddleware(['employer']), createJob);
+router.put('/:id', authMiddleware, roleMiddleware(['employer']), updateJob);
+router.delete('/:id', authMiddleware, roleMiddleware(['employer']), deleteJob);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const db = require("./db");
 
 const authRoutes = require('./routes/auth');
-// const jobRoutes = require('./routes/jobs');
+const jobRoutes = require('./routes/jobs');
 // const applicationRoutes = require('./routes/applications');
 
 const app = express();
@@ -17,7 +17,7 @@ app.use(express.json());
 
 //Routes
 app.use('/api/auth', authRoutes);
-// app.use('/api/jobs', jobRoutes);
+app.use('/api/jobs', jobRoutes);
 // app.use('/api/applications', applicationRoutes);
 
 app.listen(PORT, () =>console.log(`Server running on http://localhost:${PORT}`));
